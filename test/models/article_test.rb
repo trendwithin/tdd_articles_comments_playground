4c83f38ce1f89b3d731fb145fb920cd9ef532224
Michael Becco <e4e6d4d5@gmail.com>
2015-05-16 22:14:12 -0700

    Test Article Validation

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,11 +3,37 @@ require "test_helper"
 class ArticleTest < ActiveSupport::TestCase
 
   def article
-    @article ||= Article.new
+    @article ||= Article.new(title: 'Something Wicked This Way',
+                 body: 'Upon the eastern horizon a tumbleweed tumbled')
   end
 
   def test_valid
     assert article.valid?
+  end
+
+  def test_validity_without_title
+    article.title = '     '
+    assert_not article.valid?
+  end
+
+  def test_validity_without_body
+    article.body = '     '
+    assert_not article.valid?
+  end
+
+  def test_length_of_title
+    article.title = "a" * 255
+    assert article.valid?
+  end
+
+  def test_length_of_title_doesnt_exceed_255
+    article.title = "a" * 256
+    assert_not article.valid?
+  end
+
+  def test_length_of_article_doesnt_exceed_100000
+    article.body = "a" * 100_001
+    assert_not article.valid?
   end
 
 end
