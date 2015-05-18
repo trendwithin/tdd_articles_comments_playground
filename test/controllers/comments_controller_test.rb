d6a38fb1e70f6c361a2a024dc190a9385c9527df
Michael Becco <e4e6d4d5@gmail.com>
2015-05-18 11:05:43 -0700

    Index/Create Pass- Next Focal Point
    Connect the Article to Comment in View
    Test For Render of this view
    Create Show

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionController::TestCase
-  def test_sanity
-    flunk "Need real tests"
+  def setup
+    @article = articles(:one)
+    @comment = comments(:one)
+
+    @commentary = {
+      commenter: 'spl@ering.us',
+      body: 'we are the world'
+    }
+  end
+
+  test 'should get index' do
+    get :index, :article_id => @comment.article_id
+    assert_response :success
+    assert_not_nil assigns(:comments)
+  end
+
+  test 'should create a comment' do
+    assert_difference('Comment.count') do
+      post :create, comment: @commentary, article_id: @article.id
+    end
+
+    assert_redirected_to article_path(assigns(:article))
+    # assert_equal 'Comment was successfully created.', flash[:notice]
   end
 end
