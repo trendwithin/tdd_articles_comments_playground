54ba0937ee265d958e432a1ec27f3e6e07de2576
Michael Becco <e4e6d4d5@gmail.com>
2015-05-18 08:36:14 -0700

    Add Comment Model
    Add Relationship to Article
    Create Nested Route for Comments
    Create Controller
    Create Initial Test Stub

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 100_000 }
+
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
