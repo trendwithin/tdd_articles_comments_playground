1a6b1e2be72dc631d191c606235e3e5c0e720e00
Michael Becco <e4e6d4d5@gmail.com>
2015-05-18 11:02:29 -0700

    Add CommentController: index/new/show/create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def show
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
